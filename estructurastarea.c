1201f5363a771de27ed0633c351c1662fbb1ff9d
Juan Patino <juan.patino@udla.edu.ec>
2024-01-17 23:41:20 -0500

    Tercer paso del codigo

--- estructurastarea.c
+++ estructurastarea.c
@@ -13,3 +13,50 @@ struct alumno
 int ingresarDatos(struct alumno *estudiante);
 int mostrarDatos(struct alumno *estudiante);
 
+int main(int argc, char const *argv[])
+{
+    int n;
+    printf("Ingrese la cantidad de alumnos: ");
+    fflush(stdout);
+    scanf("%d", &n);
+
+    struct alumno estudiantes[n];
+
+    //En este apartado vamos a imprimir que opciones desea el usuario elegir.
+    int opcion;
+    do
+    {
+        printf("\n1. Ingresar datos de los alumnos\n");
+        printf("2. Mostrar datos de los alumnos\n");
+        printf("3. Salir\n");
+        printf("Seleccione una opción: ");
+        fflush(stdout);
+        scanf("%d", &opcion);
+        
+        switch (opcion)
+        {
+        case 1:
+            for (int i = 0; i < n; i++)
+            {
+                printf("\nIngrese los datos del alumno %d:\n", i + 1);
+                ingresarDatos(&estudiantes[i]);
+            }
+            break;
+        case 2:
+            printf("\nDatos de los alumnos:\n");
+            for (int i = 0; i < n; i++)
+            {
+                mostrarDatos(&estudiantes[i]);
+            }
+            break;
+        case 3:
+            printf("Saliendo del programa.\n");
+            break;
+        default:
+            printf("Opción no válida. Inténtelo de nuevo.\n");
+            break;
+        }
+    } while (opcion != 3);
+
+    return 0;
+}
